25136d9394ce7c1179698119cdcddd4aaf614a8f
camillenaulet03 <45195515+camillenaulet03@users.noreply.github.com>
2018-11-23 13:43:24 +0100

    Add files via upload

--- register.php
+++ register.php
@@ -1,22 +1,34 @@
-<?php
-$mail=(isset($_POST["mail"])) ? $_POST["mail"]:false;
-$mdp=(isset($_POST["mdp"])) ? $_POST["mdp"]:false;
-$confirm=(isset($_POST["confirm"])) ? $_POST["confirm"]:false;
-
-$lignes[] = array("mail" => $mail);
-$lignes[] = array("mdp" => $mdp);
-$lignes[] = array("confirm" => $confirm);
-$chemin = 'csv.csv';
-$delimiteur = ';';
-
-if(isset($_GET["check"])) {
-    $csv_csv = fopen($chemin, 'w+');
-    fprintf($csv_csv, chr(0xEF) . chr(0xBB) . chr(0xBF));
-    foreach ($lignes as $ligne) {
-        fputcsv($csv_csv, $ligne, $delimiteur);
-    }
-    fclose($csv_csv);
+<?php session_start();
+if(!isset($_SESSION['lignes'])) {
+    $_SESSION['lignes'] = array();
 }
+if (isset($_POST['mail']) && isset($_POST['mdp']) && isset($_POST['confirm'])) {
+    $mail = $_POST["mail"];
+    $mdp = $_POST["mdp"];
+    $confirm = $_POST["confirm"];
+    $lignes[] = array("mail" => $mail);
+    $lignes[] = array("mdp" => $mdp);
+    $confirmation[] = array("confirm" => $confirm);
+    $chemin = 'csv.csv';
+    $delimiteur = ';';
+
+    if ($mdp != $confirm){
+        echo "Votre mot de passe est différent de votre confirmation de mot de passe";
+    }
+
+    if (!empty($_POST["mail"]) && !empty($_POST["mdp"]) && !empty($_POST["confirm"])) {
+        $csv_csv = fopen($chemin, 'a+');
+        fprintf($csv_csv, chr(0xEF) . chr(0xBB) . chr(0xBF));
+        foreach ($lignes as $ligne) {
+            fputcsv($csv_csv, $ligne, $delimiteur);
+            require_once "index.php";
+        }
+        fclose($csv_csv);
+    } else {
+        echo "Merci de remplir tous les champs";
+    }
+}
+session_destroy();
 ?><!doctype html>
 <html lang="fr">
 <head>
@@ -25,22 +37,11 @@ if(isset($_GET["check"])) {
 </head>
 <body>
     <h1>Inscription</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Email</th>
-                <th>Mot de passe</th>
-                <th>Confirmation du mot de passe</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><input type="email" name="email" placeholder="Entrer votre adresse mail" ></td>
-                <td><input type="password" name="password" placeholder="Entrer votre mot de passe" ></td>
-                <td><input type="password" name="confirm" placeholder="Confirmer votre mot de passe" ></td>
-                <td><button><a class="btn btn-primary" href="?check=<?php echo $index ?>">Enregistrer</a></button></td>
-            </tr>
-            </tbody>
-    </table>
+    <form class="form-inline" action="" method="post">
+        <input class="form-control mb-2 mr_sm-2" type="email" name="mail" placeholder="Entrer votre adresse mail" >
+        <input class="form-control mb-2 mr_sm-2" type="password" name="mdp" placeholder="Entrer votre mot de passe" >
+        <input class="form-control mb-2 mr_sm-2" type="password" name="confirm" placeholder="Confirmer votre mot de passe" >
+        <button class="btn btn-primary" type="submit">Valider</button>
+    </form>
     </body>
 </html>
